b740b0e9309cab47d030426123a488120c78b1a7
Thainá Luiza <thainaluizaisrael@gmail.com>
2023-07-13 13:04:16 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+
+# Uma URL pode ser dividida em diversas partes:
+# protocolo: http
+# host: bytebank.com
+# caminho: /cambio
+# Query String: ?moedaOrigem=real&moedaDestino=dolar&quantidade=100
+
+url = 'https://bytebank.com/combio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
