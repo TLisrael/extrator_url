2a13f42d15e47ea40c8631941d26e71bd2b507df
Thainá Luiza <thainaluizaisrael@gmail.com>
2023-07-13 13:22:50 -0300

    update app.py

--- app.py
+++ app.py
@@ -5,4 +5,33 @@
 # caminho: /cambio
 # Query String: ?moedaOrigem=real&moedaDestino=dolar&quantidade=100
 
-url = 'https://bytebank.com/combio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
+url = 'bytebank.com/cambio?moedaOrigem=real'
+url_base = url[0:19]
+print(url_base)
+
+url_parametros = url[20:36]
+print(url_parametros)
+
+# A função find() retorna a posição do caractere passado como parâmetro
+# Se o caractere não for encontrado, a função retorna -1
+indice_interrogacao = url.find('?')
+print(indice_interrogacao)
+
+url_base = url[0:indice_interrogacao]
+print(url_base)
+
+# A função split() divide uma string em uma lista de strings
+# O parâmetro passado é o caractere que será usado como separador
+url_parametros = url[indice_interrogacao + 1:]
+print(url_parametros)
+
+# A função replace() substitui um caractere por outro
+url = 'bytebank.com/cambio?moedaOrigem=real'
+url = url.replace('?', '&')
+print(url)
+
+# A função split() divide uma string em uma lista de strings
+# O parâmetro passado é o caractere que será usado como separador
+url_parametros = url.split('&')
+print(url_parametros)
+
