c9ce335339a8be5c2de01863ed3677370079a4de
Thainá Luiza <thainaluizaisrael@gmail.com>
2023-07-14 10:27:01 -0300

    update app.py

--- app.py
+++ app.py
@@ -1,37 +1,48 @@
+# url = 'bytebank.com/cambio?moedaOrigem=Dolar&moedaDestino=Real&quantidade=100'
 
-# Uma URL pode ser dividida em diversas partes:
-# protocolo: http
-# host: bytebank.com
-# caminho: /cambio
-# Query String: ?moedaOrigem=real&moedaDestino=dolar&quantidade=100
 
-url = 'bytebank.com/cambio?moedaOrigem=real'
+url = " "
+
+# Sanitização de URL 
+url = url.strip() # Remove espaços no início e no fim da string
+
+# Validação de URL
+if url == "":
+    raise ValueError("A URL está vazia")
+
+# Extrai a parte da URL que contém o protocolo e o host
 url_base = url[0:19]
 print(url_base)
 
-url_parametros = url[20:36]
-print(url_parametros)
-
-# A função find() retorna a posição do caractere passado como parâmetro
-# Se o caractere não for encontrado, a função retorna -1
+# Encontra o índice do caractere '?' na URL
 indice_interrogacao = url.find('?')
-print(indice_interrogacao)
 
+# Extrai a parte da URL após o '?', que contém os parâmetros
 url_base = url[0:indice_interrogacao]
-print(url_base)
-
-# A função split() divide uma string em uma lista de strings
-# O parâmetro passado é o caractere que será usado como separador
-url_parametros = url[indice_interrogacao + 1:]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
-# A função replace() substitui um caractere por outro
-url = 'bytebank.com/cambio?moedaOrigem=real'
-url = url.replace('?', '&')
-print(url)
+# Define o parâmetro que queremos buscar na URL
+parametro_busca = 'moedaOrigem'
 
-# A função split() divide uma string em uma lista de strings
-# O parâmetro passado é o caractere que será usado como separador
-url_parametros = url.split('&')
-print(url_parametros)
+# Encontra o índice do início do parâmetro na URL
+indice_parametro = url_parametros.find(parametro_busca)
 
+# Calcula o índice do início do valor do parâmetro
+indice_valor = indice_parametro + len(parametro_busca) + 1
+
+# Encontra o índice do caractere '&' que marca o próximo parâmetro
+indice_e_comercial = url_parametros.find('&', indice_valor)
+
+# Verifica se o caractere '&' foi encontrado e extrai o valor do parâmetro
+if indice_e_comercial == -1:
+    # Se não encontrar, extrai até o final
+    valor = url_parametros[indice_valor:]
+    print(valor)
+else:
+    # Se encontrar, extrai até o caractere '&'
+    valor = url_parametros[indice_valor:indice_e_comercial]
+    print(valor)
+
+
+# O find quando nao encontrar um valor, ele retorna -1
